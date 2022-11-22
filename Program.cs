9db735a46cfb7ead76aa333ae6dbe2fda8251300
priti vyawahare <pritiv.acem@gmail.com>
2022-11-22 09:37:32 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create referance for browser
+            IWebDriver driver = new ChromeDriver();
+
+            // navigate to google page
+            driver.Navigate().GoToUrl("http://www.amazon.in");
+
+            IWebElement element = driver.FindElement(By.Id("twotabsearchtextbox"));
+
+            //perform Ops
+            element.SendKeys("Painting");
+
+            IWebElement element1 = driver.FindElement(By.Id("nav-search-submit-button"));
+
+           element1.Click();
+
+            driver.Close();
+        }
+    }
+}
